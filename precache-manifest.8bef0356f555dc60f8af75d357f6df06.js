943d61086cbf7920913dc897e11df3ffd5b64550
alexandercho <alexandercho@berkeley.edu>
2019-12-01 00:13:04 -0800

    Updates

---
+++ precache-manifest.8bef0356f555dc60f8af75d357f6df06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178d97dcdb21522e8882f237ef6d911e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0322f3a30c2c2ba2646f",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "105992e145009a9f6b52",
+    "url": "/static/js/2.676a682e.chunk.js"
+  },
+  {
+    "revision": "0322f3a30c2c2ba2646f",
+    "url": "/static/js/main.fda55789.chunk.js"
+  },
+  {
+    "revision": "4d5b4449eca69dca7740",
+    "url": "/static/js/runtime-main.77d3c816.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
