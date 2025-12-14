507baec866b1489b557774f9f861103107376b82
Alexander Cho <alexanderswcho@gmail.com>
2025-12-14 12:03:37 -0800

    data loaded

--- app/projects.tsx
+++ app/projects.tsx
@@ -1,6 +1,26 @@
 import { ThemedText } from '@/components/ThemedText';
+import { useEffect, useState } from 'react';
+
+interface Repo {
+    name: string;
+    htmlUrl: string;
+    description: string;
+    homepage: string;
+    language: string;
+}
+
 export default function Projects() {
-  return (
-    <ThemedText>UNDER CONSTRUCTION</ThemedText>
-  );
+    const [repoData, setRepoData] = useState<Repo[]>([]);
+    useEffect(() => {
+        fetch('https://api.github.com/users/alexandercho/repos')
+            .then(response => response.json())
+            .then(jsonData => jsonData
+                .map(({ name, html_url: htmlUrl, description, homepage, language }: any) => ({ name, htmlUrl, description, homepage, language })
+                )
+            )
+            .then(setRepoData)
+    }, [])
+    return (
+        <ThemedText>{JSON.stringify(repoData)}</ThemedText>
+    );
 }
