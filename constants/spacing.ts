95cf5a5f1329520451eafc73ccebe6748f01b83c
Alexander Cho <alexanderswcho@gmail.com>
2025-12-30 21:54:13 -0800

    spacing and colors

---
+++ constants/spacing.ts
@@ -0,0 +1,11 @@
+export const Spacing = {
+    none: 0,
+    xxxs: 4,   // very tight
+    xxs: 8,    // tight
+    xs: 12,    // compact
+    sm: 16,    // default
+    md: 24,    // comfortable
+    lg: 32,    // section
+    xl: 48,    // large section
+    xxl: 64   // page-level
+}
